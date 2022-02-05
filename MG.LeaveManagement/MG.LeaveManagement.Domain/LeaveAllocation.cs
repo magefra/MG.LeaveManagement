16fecc9bba7644756d084c3b6e578244feaa3414
magefra9 <magefra9@hotmail.com>
2022-02-05 13:07:46 -0600

    Agregando clases del dominio

---
+++ MG.LeaveManagement/MG.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Domain
+{
+    public class LeaveAllocation
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
