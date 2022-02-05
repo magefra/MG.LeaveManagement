d860a47a092ff100245c67852d18c96df7b21ce4
magefra9 <magefra9@hotmail.com>
2022-02-05 13:59:23 -0600

    Agregando BaseDomainEntity

--- MG.LeaveManagement/MG.LeaveManagement.Domain/LeaveRequest.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Domain/LeaveRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MG.LeaveManagement.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MG.LeaveManagement.Domain
 {
-    public class LeaveRequest
+    public class LeaveRequest : BaseDomainEntity
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
