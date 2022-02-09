778bb93d66e84e93acfe8449d13784209c4e3d33
magefra9 <magefra9@hotmail.com>
2022-02-08 19:47:34 -0600

    Agregando clase CreateLeaveTypeDtoValidator

---
+++ MG.LeaveManagement/MG.LeaveManagement.Application/Dtos/LeaveType/Validators/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Application.Dtos.LeaveType.Validators
+{
+    public class CreateLeaveTypeDtoValidator : AbstractValidator<CreateLeaveTypeDtp>
+    {
+    }
+}
