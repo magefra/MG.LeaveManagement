ae993e4ca9ab15767dd35045f793cdfafb239eae
magefra9 <magefra9@hotmail.com>
2022-02-11 19:37:48 -0600

    Agregando clases de los repositorios LeaveAllocation, LeaveRequest, LeaveType

---
+++ MG.LeaveManagement/MG.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,54 @@
+﻿using MG.LeaveManagement.Application.Persistence.Contracts;
+using MG.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        private readonly MgLeaveManagementDbContext _dbContext;
+
+        public LeaveRequestRepository(MgLeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? ApprovalStatus)
+        {
+            leaveRequest.Approved = ApprovalStatus;
+            _dbContext.Entry(leaveRequest).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            var leaveRequests = await _dbContext.LeaveRequests
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+            return leaveRequests;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+        {
+            var leaveRequests = await _dbContext.LeaveRequests.Where(q => q.RequestingEmployeeId == userId)
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+            return leaveRequests;
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _dbContext.LeaveRequests
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q => q.Id == id);
+
+            return leaveRequest;
+        }
+
+    }
+}
---
+++ MG.LeaveManagement/MG.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using MG.LeaveManagement.Application.Persistence.Contracts;
+using MG.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        private readonly MgLeaveManagementDbContext _dbContext;
+
+        public LeaveTypeRepository(MgLeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
