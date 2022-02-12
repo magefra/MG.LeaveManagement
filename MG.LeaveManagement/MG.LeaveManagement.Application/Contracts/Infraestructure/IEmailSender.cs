d2264f8f955c1a6cc1eeaa9c83e1a9840deb3e4c
magefra9 <magefra9@hotmail.com>
2022-02-12 12:38:34 -0600

    Agregando models para email  y settings

---
+++ MG.LeaveManagement/MG.LeaveManagement.Application/Contracts/Infraestructure/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Application.Contracts.Infraestructure
+{
+    public class IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+
+}
---
+++ MG.LeaveManagement/MG.LeaveManagement.Application/Models/EmailSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Application.Models
+{
+    public class EmailSettings
+    {
+        public string ApiKey { get; set; }
+
+        public string FromAddres { get; set; }
+
+        public string FronName { get; set; }
+    }
+}
