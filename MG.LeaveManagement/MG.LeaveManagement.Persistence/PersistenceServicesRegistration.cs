d23aa902d2cb965d413c6d11c0ad64313a81380e
magefra9 <magefra9@hotmail.com>
2022-02-11 19:42:50 -0600

    Agregando PersistenceServicesRegistration

---
+++ MG.LeaveManagement/MG.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,32 @@
+﻿using MG.LeaveManagement.Application.Persistence.Contracts;
+using MG.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MgLeaveManagementDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
