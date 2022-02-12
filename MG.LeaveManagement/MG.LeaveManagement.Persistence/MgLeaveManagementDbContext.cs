7a2e4d076bf4f805b892c5e84d89138ecc932f13
magefra9 <magefra9@hotmail.com>
2022-02-11 18:41:43 -0600

    Agregando DbContext

---
+++ MG.LeaveManagement/MG.LeaveManagement.Persistence/MgLeaveManagementDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Persistence
+{
+    public class MgLeaveManagementDbContext : DbContext
+    {
+    }
+}
