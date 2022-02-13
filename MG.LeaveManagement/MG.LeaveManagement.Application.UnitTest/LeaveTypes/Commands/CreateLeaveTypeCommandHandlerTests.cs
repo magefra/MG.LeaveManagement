fd4deacfcd6800197e66b62ffaa5bc3dd8a9907d
magefra9 <magefra9@hotmail.com>
2022-02-13 10:43:03 -0600

    Agregando CreateLeaveTypeCommandHandlerTests

--- MG.LeaveManagement/MG.LeaveManagement.Application.UnitTest/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Application.UnitTest/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -2,7 +2,10 @@
 using MG.LeaveManagement.Application.Dtos.LeaveType;
 using MG.LeaveManagement.Application.Feactures.LeaveTypes.Handlers.Commands;
 using MG.LeaveManagement.Application.Feactures.LeaveTypes.Requests.Commands;
+using MG.LeaveManagement.Application.Profiles;
 using MG.LeaveManagement.Application.Responses;
+using MG.LeaveManagement.Application.UnitTest.Mocks;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,19 +19,20 @@ namespace MG.LeaveManagement.Application.UnitTest.LeaveTypes.Commands
     public class CreateLeaveTypeCommandHandlerTests
     {
         private readonly IMapper _mapper;
+        private readonly Mock<Contracts.Persistence.ILeaveTypeRepository> _mockRepo;
         private readonly CreateLeaveTypeDtp _leaveTypeDto;
         private readonly CreateLeaveTypeCommandHandler _handler;
 
         public CreateLeaveTypeCommandHandlerTests()
         {
-            //_mockUow = MockUnitOfWork.GetUnitOfWork();
+            _mockRepo = MockLeaveTypeRepository.GetLeaveTypeRepository();
 
-            //var mapperConfig = new MapperConfiguration(c =>
-            //{
-            //    c.AddProfile<MappingProfile>();
-            //});
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
 
-            //_mapper = mapperConfig.CreateMapper();
+            _mapper = mapperConfig.CreateMapper();
             //_handler = new CreateLeaveTypeCommandHandler(_mockUow.Object, _mapper);
 
             //_leaveTypeDto = new CreateLeaveTypeDto
