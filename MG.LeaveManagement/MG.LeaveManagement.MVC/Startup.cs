13d5a860109154095e45aea0697055f6a1ed5b82
magefra9 <magefra9@hotmail.com>
2022-02-16 02:00:52 -0600

    Agregan ServiceCliente alproyecto mvc

--- MG.LeaveManagement/MG.LeaveManagement.MVC/Startup.cs
+++ MG.LeaveManagement/MG.LeaveManagement.MVC/Startup.cs
@@ -23,6 +23,9 @@ namespace MG.LeaveManagement.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+
             services.AddControllersWithViews();
         }
 
