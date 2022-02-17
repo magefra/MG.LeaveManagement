b84ab60279eb2fd82a740936fd2dbba7ac681482
magefra9 <magefra9@hotmail.com>
2022-02-17 10:46:23 -0600

    Agregando LocalStorageService

---
+++ MG.LeaveManagement/MG.LeaveManagement.MVC/Services/LocalStorageService.cs
@@ -0,0 +1,46 @@
+﻿using Hanssens.Net;
+using MG.LeaveManagement.MVC.Contracts;
+using System.Collections.Generic;
+
+namespace MG.LeaveManagement.MVC.Services
+{
+    public class LocalStorageService : ILocalStorageService
+    {
+        private LocalStorage _storage;
+
+        public LocalStorageService()
+        {
+            var config = new LocalStorageConfiguration()
+            {
+                AutoLoad = true,
+                AutoSave = true,
+                Filename = "HR.LEAVEMGMT"
+            };
+            _storage = new LocalStorage(config);
+        }
+
+        public void ClearStorage(List<string> keys)
+        {
+            foreach (var key in keys)
+            {
+                _storage.Remove(key);
+            }
+        }
+
+        public void SetStorageValue<T>(string key, T value)
+        {
+            _storage.Store(key, value);
+            _storage.Persist();
+        }
+
+        public T GetStorageValue<T>(string key)
+        {
+            return _storage.Get<T>(key);
+        }
+
+        public bool Exists(string key)
+        {
+            return _storage.Exists(key);
+        }
+    }
+}
