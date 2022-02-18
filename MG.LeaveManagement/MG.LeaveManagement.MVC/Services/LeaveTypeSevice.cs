605e9d14159f758c4905a18becc691c141300488
magefra9 <magefra9@hotmail.com>
2022-02-18 10:11:35 -0600

    Agregando implementación a LeaveAllocationService, LeaveRequestService y LeaveTypeService.

--- MG.LeaveManagement/MG.LeaveManagement.MVC/Services/LeaveTypeSevice.cs
+++ MG.LeaveManagement/MG.LeaveManagement.MVC/Services/LeaveTypeSevice.cs
@@ -1,8 +1,97 @@
-﻿using MG.LeaveManagement.MVC.Contracts;
+﻿using AutoMapper;
+using MG.LeaveManagement.MVC.Contracts;
+using MG.LeaveManagement.MVC.Models;
+using MG.LeaveManagement.MVC.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MG.LeaveManagement.MVC.Services;
 
 namespace MG.LeaveManagement.MVC.Services
 {
-    public class LeaveTypeSevice : IleaveTypeService
+    public class LeaveTypeSevice : BaseHttpService, IleaveTypeService
     {
+        private readonly ILocalStorageService _localStorageService;
+        private readonly IMapper _mapper;
+        private readonly IClient _httpclient;
+
+        public LeaveTypeSevice(IMapper mapper, IClient httpclient, ILocalStorageService localStorageService) : base(httpclient, localStorageService)
+        {
+            this._localStorageService = localStorageService;
+            this._mapper = mapper;
+            this._httpclient = httpclient;
+        }
+
+        public async Task<Response<int>> CreateLeaveType(CreateLeaveTypeVM leaveType)
+        {
+            try
+            {
+                var response = new Response<int>();
+                CreateLeaveTypeDtp createLeaveType = _mapper.Map<CreateLeaveTypeDtp>(leaveType);
+                AddBearerToken();
+                var apiResponse = await _client.LeaveTypesPOSTAsync(createLeaveType);
+                if (apiResponse.Success)
+                {
+                    response.Data = apiResponse.Id;
+                    response.Success = true;
+                }
+                else
+                {
+                    foreach (var error in apiResponse.Errors)
+                    {
+                        response.ValidationErrors += error + Environment.NewLine;
+                    }
+                }
+                return response;
+            }
+            catch (ApiException ex)
+            {
+                return ConvertApiExceptions<int>(ex);
+            }
+        }
+
+        public async Task<Response<int>> DeleteLeaveType(int id)
+        {
+            try
+            {
+                AddBearerToken();
+                await _client.LeaveTypesDELETEAsync(id);
+                return new Response<int>() { Success = true };
+            }
+            catch (ApiException ex)
+            {
+                return ConvertApiExceptions<int>(ex);
+            }
+        }
+
+        public async Task<LeaveTypeVM> GetLeaveTypeDetails(int id)
+        {
+            AddBearerToken();
+            var leaveType = await _client.LeaveTypesGETAsync(id);
+            return _mapper.Map<LeaveTypeVM>(leaveType);
+        }
+
+        public async Task<List<LeaveTypeVM>> GetLeaveTypes()
+        {
+            AddBearerToken();
+            var leaveTypes = await _client.LeaveTypesAllAsync();
+            return _mapper.Map<List<LeaveTypeVM>>(leaveTypes);
+        }
+
+        public async Task<Response<int>> UpdateLeaveType(int id, LeaveTypeVM leaveType)
+        {
+            try
+            {
+                LeaveTypeDto leaveTypeDto = _mapper.Map<LeaveTypeDto>(leaveType);
+                AddBearerToken();
+                await _client.LeaveTypesPUTAsync(id.ToString(), leaveTypeDto);
+                return new Response<int>() { Success = true };
+            }
+            catch (ApiException ex)
+            {
+                return ConvertApiExceptions<int>(ex);
+            }
+        }
+
     }
 }
