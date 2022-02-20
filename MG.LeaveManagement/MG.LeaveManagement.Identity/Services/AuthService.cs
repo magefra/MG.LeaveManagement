03924a9fd783701e715f217c9a71a3fc736898aa
magefra9 <magefra9@hotmail.com>
2022-02-20 01:57:49 -0600

    Agregando clase AuthService

---
+++ MG.LeaveManagement/MG.LeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using MG.LeaveManagement.Application.Contracts.Identity;
+using MG.LeaveManagement.Application.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<AuthResponse> Login(AuthRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
