079b563b86cce9c946d18fc04e1eaa5dda1789e9
magefra9 <magefra9@hotmail.com>
2022-02-20 01:58:30 -0600

    Agregando UserService

---
+++ MG.LeaveManagement/MG.LeaveManagement.Identity/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using MG.LeaveManagement.Application.Contracts.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.Identity.Services
+{
+    public class UserService : IUserService
+    {
+    }
+}
