fed6e916a45f2be09cb2e72eddeef7c018a474f0
magefra9 <magefra9@hotmail.com>
2022-02-24 18:30:12 -0600

    Agregando RegisterVM

---
+++ MG.LeaveManagement/MG.LeaveManagement.MVC/Contracts/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using MG.LeaveManagement.MVC.Models;
+using System.Threading.Tasks;
+
+namespace MG.LeaveManagement.MVC.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Authenticate(string email, string password);
+        Task<bool> Register(RegisterVM registration);
+        Task Logout();
+    }
+}
