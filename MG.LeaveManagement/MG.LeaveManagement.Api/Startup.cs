5c83911d51cf1de5eda75dab52e76fc167bc1208
magefra9 <magefra9@hotmail.com>
2022-03-01 16:50:21 -0600

    Inyectando IHttpContextAccessor

--- MG.LeaveManagement/MG.LeaveManagement.Api/Startup.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Api/Startup.cs
@@ -25,7 +25,7 @@ namespace MG.LeaveManagement.Api
         public void ConfigureServices(IServiceCollection services)
         {
 
-
+            services.AddHttpContextAccessor();
             AddSwaggerDoc(services);
 
             services.AddControllers();
--- MG.LeaveManagement/MG.LeaveManagement.Application/Feactures/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Application/Feactures/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -1,12 +1,16 @@
 ﻿using AutoMapper;
 using MediatR;
+using MG.LeaveManagement.Application.Constants;
 using MG.LeaveManagement.Application.Contracts.Infraestructure;
 using MG.LeaveManagement.Application.Contracts.Persistence;
 using MG.LeaveManagement.Application.Dtos.LeaveRequest.Validators;
 using MG.LeaveManagement.Application.Models;
 using MG.LeaveManagement.Application.Responses;
 using MG.LeaveManagement.Domain;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,64 +18,70 @@ namespace MG.LeaveManagement.Application.Feactures.LeaveRequests.Handlers.Comman
 {
     public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, BaseCommandResponse>
     {
-        private readonly ILeaveRequestRepository _leaveTypeRepository;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly ILeaveTypeRepository leaveTypeRepository1;
         private readonly IEmailSender _emailSender;
         private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveTypeRepository,
             ILeaveTypeRepository leaveTypeRepository1,
             IEmailSender emailSender,
+            IHttpContextAccessor httpContextAccessor,
             IMapper mapper)
         {
-            _leaveTypeRepository = leaveTypeRepository;
+            _leaveRequestRepository = leaveTypeRepository;
             this.leaveTypeRepository1 = leaveTypeRepository1;
             _emailSender = emailSender;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
         {
             var response = new BaseCommandResponse();
             var validator = new CreateLeaveRequestDtoValidators(leaveTypeRepository1);
-            var validatorResult = await validator.ValidateAsync(request.LeaveRequestDto);
+            var validationResult = await validator.ValidateAsync(request.LeaveRequestDto);
+            var userId = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(
+                   q => q.Type == CustomClaimTypes.Uid)?.Value;
 
-            if (!validatorResult.IsValid)
+
+            if (validationResult.IsValid == false)
             {
                 response.Success = false;
-                response.Message = "Creation Failed";
-                response.Errors = validatorResult.Errors.Select(x => x.ErrorMessage).ToList();
+                response.Message = "Request Failed";
+                response.Errors = validationResult.Errors.Select(q => q.ErrorMessage).ToList();
             }
-
-
-            var leaveType = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
-
-            leaveType = await _leaveTypeRepository.Add(leaveType);
-
-
-            response.Success = true;
-            response.Message = "Creation Successful";
-            response.Id = leaveType.Id;
-
-
-            var email = new Email
+            else
             {
-                To = "magefra9@hotmail.com",
-                Body = $"Your leave request for {request.LeaveRequestDto.StartDate:D} to {request.LeaveRequestDto.EndDate:D} " +
+                var leaveRequest = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
+                leaveRequest.RequestingEmployeeId = userId;
+                leaveRequest = await _leaveRequestRepository.Add(leaveRequest);
+                //await _unitOfWork.Save();
+
+                response.Success = true;
+                response.Message = "Request Created Successfully";
+                response.Id = leaveRequest.Id;
+
+                try
+                {
+                    var emailAddress = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Email).Value;
+
+                    var email = new Email
+                    {
+                        To = emailAddress,
+                        Body = $"Your leave request for {request.LeaveRequestDto.StartDate:D} to {request.LeaveRequestDto.EndDate:D} " +
                         $"has been submitted successfully.",
-                Subject = "Leave Request Submitted"
-            };
+                        Subject = "Leave Request Submitted"
+                    };
 
-            try
-            {
-                await _emailSender.SendEmail(email);
+                    await _emailSender.SendEmail(email);
+                }
+                catch (Exception ex)
+                {
+                    //// Log or handle error, but don't throw...
+                }
             }
-            catch (System.Exception ex)
-            {
-
-                throw;
-            }
-
 
             return response;
         }
