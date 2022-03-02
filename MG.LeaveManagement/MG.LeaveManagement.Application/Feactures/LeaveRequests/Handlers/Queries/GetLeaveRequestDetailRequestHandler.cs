b9be5a296cb00b738eeb9c80a6ade9f86da7715c
magefra9 <magefra9@hotmail.com>
2022-03-02 13:28:30 -0600

    Modificando GetLeaveRequestDetailRequestHandler

--- MG.LeaveManagement/MG.LeaveManagement.Application/Feactures/LeaveRequests/Handlers/Queries/GetLeaveRequestDetailRequestHandler.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Application/Feactures/LeaveRequests/Handlers/Queries/GetLeaveRequestDetailRequestHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using MG.LeaveManagement.Application.Contracts.Identity;
 using MG.LeaveManagement.Application.Contracts.Persistence;
 using MG.LeaveManagement.Application.Dtos.LeaveRequest;
 using MG.LeaveManagement.Application.Feactures.LeaveRequests.Request.Queries;
@@ -12,17 +13,21 @@ namespace MG.LeaveManagement.Application.Feactures.LeaveRequests.Handlers.Querie
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly IMapper _mapper;
+        private readonly IUserService _userService;
 
         public GetLeaveRequestDetailRequestHandler(ILeaveRequestRepository leaveRequestRepository,
-            IMapper mapper)
+            IMapper mapper,
+            IUserService userService)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
+            this._userService = userService;
         }
 
         public async Task<LeaveRequestDto> Handle(GetLeaveRequestDetailRequest request, CancellationToken cancellationToken)
         {
             var leaveRequest = _mapper.Map<LeaveRequestDto>(await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
+            leaveRequest.Employee = await _userService.GetEmployee(leaveRequest.RequestingEmployeeId);
             return leaveRequest;
         }
     }
