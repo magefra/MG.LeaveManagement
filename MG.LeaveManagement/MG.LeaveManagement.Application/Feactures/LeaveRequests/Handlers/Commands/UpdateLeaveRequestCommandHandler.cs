0723cb8fec7c73424ba3ae73dccf79846d0df42c
magefra9 <magefra9@hotmail.com>
2022-03-03 18:02:55 -0600

    Modificando UpdateLeaveRequestCommandHandler

--- MG.LeaveManagement/MG.LeaveManagement.Application/Feactures/LeaveRequests/Handlers/Commands/UpdateLeaveRequestCommandHandler.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Application/Feactures/LeaveRequests/Handlers/Commands/UpdateLeaveRequestCommandHandler.cs
@@ -10,48 +10,44 @@ namespace MG.LeaveManagement.Application.Feactures.LeaveRequests.Handlers.Comman
 {
     public class UpdateLeaveRequestCommandHandler : IRequestHandler<UpdateLeaveRequestCommand, Unit>
     {
-        private readonly ILeaveRequestRepository _leaveRequestRepository;
-        private readonly ILeaveTypeRepository leaveTypeRepository;
-        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+       
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository,
-            ILeaveTypeRepository leaveTypeRepository,
-            ILeaveAllocationRepository leaveAllocationRepository,
+        public UpdateLeaveRequestCommandHandler(IUnitOfWork unitOfWork,
             IMapper mapper)
         {
-            _leaveRequestRepository = leaveRequestRepository;
-            _leaveAllocationRepository = leaveAllocationRepository;
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
 
         public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
         {
-            var leaveRequest= await _leaveRequestRepository.Get(request.Id);
+            var leaveRequest= await _unitOfWork.LeaveRequestRepository.Get(request.Id);
 
             if (request.LeaveRequestDto != null)
             {
-                var validator = new UpdateLeaveRequestDtoValidator(leaveTypeRepository);
+                var validator = new UpdateLeaveRequestDtoValidator(_unitOfWork.LeaveTypeRepository);
                 var validationResult = await validator.ValidateAsync(request.LeaveRequestDto);
                 if (validationResult.IsValid == false)
                     throw new ValidationException(validationResult);
                 _mapper.Map(request.LeaveRequestDto, leaveRequest);
 
 
-                await _leaveRequestRepository.Update(leaveRequest);
+                await _unitOfWork.LeaveRequestRepository.Update(leaveRequest);
             }
             else if(request.ChangeLeaveRequestApprovalDto != null)
             {
 
-                await _leaveRequestRepository.ChangeApprovalStatus(leaveRequest, request.ChangeLeaveRequestApprovalDto.Approved);
+                await _unitOfWork.LeaveRequestRepository.ChangeApprovalStatus(leaveRequest, request.ChangeLeaveRequestApprovalDto.Approved);
                 if (request.ChangeLeaveRequestApprovalDto.Approved.Value)
                 {
-                    var allocation = await _leaveAllocationRepository.GetUserAllocations(leaveRequest.RequestingEmployeeId, leaveRequest.LeaveTypeId);
+                    var allocation = await _unitOfWork.LeaveAllocationRepository.GetUserAllocations(leaveRequest.RequestingEmployeeId, leaveRequest.LeaveTypeId);
                     int daysRequested = (int)(leaveRequest.EndDate - leaveRequest.StartDate).TotalDays;
 
                     allocation.NumberOfDays -= daysRequested;
 
-                    await _leaveAllocationRepository.Update(allocation);
+                    await _unitOfWork.LeaveAllocationRepository.Update(allocation);
                 }
 
             }
--- MG.LeaveManagement/MG.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
+++ MG.LeaveManagement/MG.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -16,6 +16,7 @@ namespace MG.LeaveManagement.Persistence
 
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
             services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
